1e496b38f9d04b6479295328bb1a8ae907c00a63
zmkva <2589529538@qq.com>
2023-04-06 15:02:59 +0000

    Adapt to higher system versions: Linux 6.2.10-060210-generic

--- src/os/unix/ngx_user.c
+++ src/os/unix/ngx_user.c
@@ -23,7 +23,7 @@ ngx_libc_crypt(ngx_pool_t *pool, u_char *key, u_char *salt, u_char **encrypted)
     cd.initialized = 0;
 #ifdef __GLIBC__
     /* work around the glibc bug */
-    cd.current_salt[0] = ~salt[0];
+//    cd.current_salt[0] = ~salt[0];
 #endif
 
     value = crypt_r((char *) key, (char *) salt, &cd);
